679d6274f27d898f7a52473531892eca322e6a8a
James Hawk <jph94880@gmail.com>
2023-07-03 11:18:38 -0500

    Adds my file

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -14,18 +14,3 @@ function CategoryFilter({ categories }) {
 
 export default CategoryFilter;
 
-// import React from "react";
-
-
-// function CategoryFilter({ categories }) {
-//   return (
-//     <div className="categories">
-//       <h5>Category filters</h5>
-//       {categories.map((category, index) => (
-//         <button key={index}>{category}</button>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default CategoryFilter;
